8fc88f1dbaeb7a86bc52ea1aa27b71690c78879b
Anastasia <evseev-lomb@mail.ru>
2022-11-02 22:40:51 +0300

    Homework3.3

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача1
+/*Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] && number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите правильное число");*/
+
+//Задача2
+/*double FindDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+   return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+}
+Console.WriteLine("Input x1:");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input y1:");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input z1:");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input x2:");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input y2:");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input z2:");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double findDistance = FindDistance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Distance ={findDistance} ");*/
+
+//Задача3
+
+/*void  quad (int num)
+
+{
+    int step = 1;
+
+    while (step <= num)
+    {
+
+ Console.WriteLine($"куб числа { step } равен {step * step * step}");
+  
+  step ++;
+ } 
+
+}
+Console.WriteLine("input num");
+int num = Convert.ToInt32(Console.ReadLine());
+
+quad(num); 
+*/
